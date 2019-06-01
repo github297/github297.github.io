42c43ec52a25b0306384a24b73433f3a0d1be72a
Justin Faust Account <fau13011@byui.edu>
2019-06-01 00:24:14 -0400

    s

--- Weather Site/scripts/currentdate.js
+++ Weather Site/scripts/currentdate.js
@@ -1,10 +1,10 @@
 function currentDate()
 {
-var today = newDate();
-var day = String(today.getDate()).padStart(2,'0');
-var number = newDate();
-var month = String(today.getMonth() +1).padStart(2,'0');
-var year = today.getFullYear();   
+    var today = newDate();
+    var day = String(today.getDate()).padStart(2,'0');
+    var number = newDate();
+    var month = String(today.getMonth() +1).padStart(2,'0');
+    var year = today.getFullYear();   
     
 today = day + ', ' + number + month + year;
 document.write(today);
