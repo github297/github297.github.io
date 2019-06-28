eb2aa67714f745653670736a7dbdb66a7893cf6a
Justin Faust Account <fau13011@byui.edu>
2019-06-27 22:02:37 -0400

    s

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -12,5 +12,5 @@ document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 const iconcode = weatherInfo.weather[0].icon;
 console.log(iconcode);
 
-
+const icon_path = "openweathermap.org/img/w/" + iconcode + ".png";
 } //end of onload function
