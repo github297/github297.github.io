37e62cea874832a8d0606f25b22c335bd8377fe4
Justin Faust Account <fau13011@byui.edu>
2019-06-28 20:51:16 -0400

    a

--- Weather Site/scripts/towns.js
+++ Weather Site/scripts/towns.js
@@ -1,18 +1,18 @@
 //EXAMPLE BY BROTHER CHENEY
 
 //create an object
-const myRequestObject = new XMLHttpRequest();
+const requestObject = new XMLHttpRequest();
 
 //enter the path to the local json file
-const myRequestURL = "townData.json";
+const requestURL = "townData.json";
 
 //Now we will create a global container called myWeatherInfo which can be used anywhere in the script.
-let myWeatherInfo;
-myRequestObject.open("GET", myRequestURL);
-myRequestObject.send();
-myRequestObject.onload = function() {
-    myWeatherInfo = JSON.parse(myRequestObject.responseText);
-    console.log(myWeatherInfo);
+let weatherInfo;
+requestObject.open("GET", requestURL);
+requestObject.send();
+requestObject.onload = function() {
+    weatherInfo = JSON.parse(requestObject.responseText);
+    console.log(weatherInfo);
 	
 	//make a call to the function below for each town and send the array number of the needed town. Ppon returning insert it as html inside the id for each city
 	document.getElementById("fish").innerHTML = assembleData(1);
@@ -25,18 +25,18 @@ function assembleData(x) {
 	console.log(myWeatherInfo.towns[x]);
 	
 	//Build the html as a javascript variable and include the html tags
-	let townInfo = "<h2>"+myWeatherInfo.towns[x].name+"</h2>";
-	townInfo += "<i>"+myWeatherInfo.towns[x].motto+"</i>";
-	townInfo += "<p>Year founded: "+myWeatherInfo.towns[x].yearFounded+"</p>";
-	townInfo += "<p>Current Population: "+myWeatherInfo.towns[x].currentPopulation+"</p>";
-	townInfo += "<p>Rainfall: "+myWeatherInfo.towns[x].averageRainfall+"</p>";
+	let townInfo = "<h3>"+weatherInfo.towns[x].name+"</h3>";
+	townInfo += "<i>"+weatherInfo.towns[x].motto+"</i>";
+	townInfo += "<p>Year founded: "+weatherInfo.towns[x].yearFounded+"</p>";
+	townInfo += "<p>Current Population: "+weatherInfo.towns[x].currentPopulation+"</p>";
+	townInfo += "<p>Rainfall: "+weatherInfo.towns[x].averageRainfall+"</p>";
          
 	//return the variable that now has all the town information
 	return townInfo;
 }
 
-let townNames = jsonObj["towns"][1],["towns"][4],["towns"][5];
-console.log(townNames);
+l//et townNames = jsonObj["towns"][1],["towns"][4],["towns"][5];
+//console.log(townNames);
 
 // help from Justin Schone
 //var towndata = townRequest.response;
