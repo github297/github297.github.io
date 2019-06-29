67988d130312d0614142752390fc7a49c1b1ac94
Justin Faust Account <fau13011@byui.edu>
2019-06-29 16:32:01 -0400

    d

--- Weather Site/scripts/weatherInfo.js
+++ Weather Site/scripts/weatherInfo.js
@@ -4,6 +4,7 @@ weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText)
     console.log(weatherInfo);
+
 document.getElementById('description').innerHTML = weatherInfo.weather[0].description
 document.getElementById('place').innerHTML = weatherInfo.name;
 document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
