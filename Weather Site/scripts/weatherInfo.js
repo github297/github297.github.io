be175ca1d34b6c6bbf3445c2d4df0ad4c32ff377
Justin Faust Account <fau13011@byui.edu>
2019-06-28 22:09:24 -0400

    a

--- Weather Site/scripts/weatherInfo.js
+++ Weather Site/scripts/weatherInfo.js
@@ -1,6 +1,6 @@
 const weatherObject = new XMLHttpRequest();
 
-weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=83127, us&appid=0e2291c05d0d27dffb42a2effa870708", true );
+weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?zip=83127, us&appid=0e2291c05d0d27dffb42a2effa870708", true );
 
 weatherObject.send();
 weatherObject.onload = function() {
