b410188883e2d04c79c0834e44752ffcd5d19fd0
Justin Faust Account <fau13011@byui.edu>
2019-07-06 19:33:03 -0400

    d

--- Weather Site/scripts/5dayweatherInfo.js
+++ Weather Site/scripts/5dayweatherInfo.js
@@ -7,35 +7,47 @@ newweatherObject.onload = function() {
     console.log(newweatherInfo);
 
 //declare variables for the days of the week. I'm still working on figuring out this part out.
-    var day = new Date();
-    var today = day.getDay();
-    var tomorrow =+ today;
+function dayNames {
+var day = new Date();
+    var weekday = new array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
 
-document.getElementById('day1').innerHTML = tomorrow;
+var n = weekday[day.getDay()];
+document.getElementById("output").innerHTML = n;
+
+}
+
+document.getElementById('output').innerHTML = n;
 document.getElementById('desc1').innerHTML = newweatherInfo.list[6].weather[0].description
 document.getElementById('high1').innerHTML = newweatherInfo.list[6].main.temp_max;
 document.getElementById('low1').innerHTML = newweatherInfo.list[6].main.temp_min;
 document.getElementById('wind1').innerHTML = newweatherInfo.list[6].wind.speed;
 
-document.getElementById('day2').innerHTML = tomorrow;
+document.getElementById('day2').innerHTML = n;
 document.getElementById('desc2').innerHTML = newweatherInfo.list[14].weather[0].description
 document.getElementById('high2').innerHTML = newweatherInfo.list[14].main.temp_max;
 document.getElementById('low2').innerHTML = newweatherInfo.list[14].main.temp_min;
 document.getElementById('wind2').innerHTML = newweatherInfo.list[14].wind.speed;
 
-document.getElementById('day3').innerHTML = tomorrow;
+document.getElementById('day3').innerHTML = n;
 document.getElementById('desc3').innerHTML = newweatherInfo.list[22].weather[0].description
 document.getElementById('high3').innerHTML = newweatherInfo.list[22].main.temp_max;
 document.getElementById('low3').innerHTML = newweatherInfo.list[22].main.temp_min;
 document.getElementById('wind3').innerHTML = newweatherInfo.list[22].wind.speed;
 
-document.getElementById('day4').innerHTML = tomorrow;
+document.getElementById('day4').innerHTML = n;
 document.getElementById('desc4').innerHTML = newweatherInfo.list[30].weather[0].description
 document.getElementById('high4').innerHTML = newweatherInfo.list[30].main.temp_max;
 document.getElementById('low4').innerHTML = newweatherInfo.list[30].main.temp_min;
 document.getElementById('wind4').innerHTML = newweatherInfo.list[30].wind.speed;
 
-document.getElementById('day5').innerHTML = tomorrow;
+document.getElementById('day5').innerHTML = n;
 document.getElementById('desc5').innerHTML = newweatherInfo.list[38].weather[0].description
 document.getElementById('high5').innerHTML = newweatherInfo.list[38].main.temp_max;
 document.getElementById('low5').innerHTML = newweatherInfo.list[38].main.temp_min;
