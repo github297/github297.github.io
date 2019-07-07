5f28905a78b520ffe7bb372c26bf124b859f30fc
Justin Faust Account <fau13011@byui.edu>
2019-07-06 20:51:54 -0400

    d

--- Weather Site/scripts/windchill.js
+++ Weather Site/scripts/windchill.js
@@ -2,6 +2,6 @@ let tempF = parseInt(document.getElementById("maxTemp").innerHTML);
 
 let speed = parseInt(document.getElementById("windSpeed").innerHTML);
 
-var formula = 35.74 + 0.6215* tempF + (0.4275*tempF - 35.75)* Math.pow(speed, 0.16); 
+var formula = 35.74 + 0.6215 * tempF + (0.4275 * tempF - 35.75) * Math.pow(speed, 0.16); 
 
 document.getElementById("wchill").innerHTML = formula;
