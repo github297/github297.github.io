8f02d230f08296f70169b523c9dc7bb87690d28e
Justin Faust Account <fau13011@byui.edu>
2019-07-06 20:21:42 -0400

    d

--- Weather Site/scripts/windchill.js
+++ Weather Site/scripts/windchill.js
@@ -1,7 +1,7 @@
-function windChill()
-{
-    var temp = 36;
-    var chill = 87;
-    var result = 36.74 + 0.6215 temp - 35.75 chill^0.16 + 0.4275 temp chill^0.16;
+let temp = parseInt(document.getElementById("maxTemp").innerHTML);
 
-    document.getElementById("wchill").innerHTML = result;
+let chill = parseInt(document.getElementById("windspeed").innerHTML);
+
+var result = 36.74 + (0.6215 * temp) - (35.75 * (chill * 0.16 + (0.4275 * temp * (chill * 0.16);
+
+document.getElementById("wchill").innerHTML = result;
