9fc066b618a817bbe3e36750f83d53215cfd2b26
Justin Faust Account <fau13011@byui.edu>
2019-07-25 19:27:33 -0400

    a

--- Temple Inn & Suites/scripts/weatherInfo.js
+++ Temple Inn & Suites/scripts/weatherInfo.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=0e2291c05d0d27dffb42a2effa870708&units=imperial", true )
+weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?zip=20895,us&appid=0e2291c05d0d27dffb42a2effa870708&units=imperial", true )
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText)
@@ -17,3 +17,64 @@ console.log(icon_path);
 
 document.getElementById('weather_icon').src = icon_path;
 }
+
+const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?zip=19103,us&appid=0e2291c05d0d27dffb42a2effa870708&units=imperial", true )
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText)
+    console.log(weatherInfo);
+
+document.getElementById('description2').innerHTML = weatherInfo.weather[0].description
+document.getElementById('currentTemp2').innerHTML = weatherInfo.main.temp;
+document.getElementById('windSpeed2').innerHTML = weatherInfo.wind.speed;
+document.getElementById('humidity2').innerHTML = weatherInfo.main.humidity;
+
+const iconcode = weatherInfo.weather[0].icon;
+
+const icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+console.log(icon_path);
+
+document.getElementById('weather_icon2').src = icon_path;
+}
+
+const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?zip=02487,us&appid=0e2291c05d0d27dffb42a2effa870708&units=imperial", true )
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText)
+    console.log(weatherInfo);
+
+document.getElementById('description3').innerHTML = weatherInfo.weather[0].description
+document.getElementById('currentTemp3').innerHTML = weatherInfo.main.temp;
+document.getElementById('windSpeed3').innerHTML = weatherInfo.wind.speed;
+document.getElementById('humidity3').innerHTML = weatherInfo.main.humidity;
+
+const iconcode = weatherInfo.weather[0].icon;
+
+const icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+console.log(icon_path);
+
+document.getElementById('weather_icon3').src = icon_path;
+}
+
+const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?zip=10023,us&appid=0e2291c05d0d27dffb42a2effa870708&units=imperial", true )
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText)
+    console.log(weatherInfo);
+
+document.getElementById('description4').innerHTML = weatherInfo.weather[0].description
+document.getElementById('currentTemp4').innerHTML = weatherInfo.main.temp;
+document.getElementById('windSpeed4').innerHTML = weatherInfo.wind.speed;
+document.getElementById('humidity4').innerHTML = weatherInfo.main.humidity;
+
+const iconcode = weatherInfo.weather[0].icon;
+
+const icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+console.log(icon_path);
+
+document.getElementById('weather_icon4').src = icon_path;
+}
+
